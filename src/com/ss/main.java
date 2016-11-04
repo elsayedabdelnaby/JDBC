c871c96d7374ff3224d011acc2bd9822524778a2
DESKTOP-7NQ67K9\elsayed <elsayed.fcis.1@gmail.com>
2016-11-04 13:46:51 +0200

    crate DB Connection

---
+++ src/com/ss/main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ss;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author elsayed
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        DBUtil.getConnection(DBType.MYSQL);
+        System.out.println("connection success");
+    }
+    
+}
